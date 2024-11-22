f2471624a73c34c89076e1b2a6a844da66aeb561
Lvxkyispro <158829858+Lvxkyispro@users.noreply.github.com>
2024-11-22 16:50:42 +0530

    Update pro.py

--- pro.py
+++ pro.py
@@ -13,7 +13,7 @@ import html
 import sys
 
 # Replace 'YOUR_BOT_TOKEN' with your actual Telegram bot token
-bot_token = '7560925100:AAFFkmePdsufNu3fH4DVsKRwOGkIYJMJW7A'
+bot_token = '7560925100:AAHPBCKPip4ef7pL8pN0rhXue2Mabdv3T4E'
 bot = telebot.TeleBot(bot_token)
 
 @bot.message_handler(commands=['start'])
