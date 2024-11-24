1358a710c271b65aaa84c8f189e6b91d4d0c1314
Lvxkyispro <158829858+Lvxkyispro@users.noreply.github.com>
2024-11-24 14:28:39 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+# Bot Configuration
+BOT_TOKEN = '7920311542:AAGsX2Q60J-mFUm2Oj9o4KWJSNppUx3y_5M'  # Replace with your actual bot token
+BOT_USERNAME = 'YourBotUsername'  # Replace with your bot's username
+
+# API Keys and Tokens
+GOOGLE_API_KEY = "AIzaSyAKpgHmVRe2qlOP01jc0uiazCtWVTSHi5M"
+SEARCH_ENGINE_ID = "b341ba8ffe8ee4a14"
+IPINFO_TOKEN = "4dd04ebcd1b3c1"
+
+
+
